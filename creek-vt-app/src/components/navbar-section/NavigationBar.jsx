b99819febfb37886982369eeacb24545a8154838
Alex Villanueva <138938590+liminalpup@users.noreply.github.com>
2023-09-08 09:19:39 -0400

    react app created for frontend

---
+++ creek-vt-app/src/components/navbar-section/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavigationBar.css';
+
+
+
+
+
+const NavigationBar = (props) => {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default NavigationBar;
