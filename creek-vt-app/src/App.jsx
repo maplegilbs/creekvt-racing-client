994d6c72071cb85a26ef6fe40fe8797715fe04d8
Matt Baum <mattbaum44@gmail.com>
2023-09-13 13:19:57 -0400

    added incomplete photogallery - working on media query for mobile compatibility.

--- creek-vt-app/src/App.jsx
+++ creek-vt-app/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/navbar-section/NavigationBar";
+import PhotoGallery from "./components/photo-gal-section/PhotoGallery";
 
-function App() {
+function App({Component, pageProps}) {
   const [token, setToken] = useState("");
-
+  
   function updateToken(newToken) {
     setToken(newToken);
     localStorage.setItem("token", newToken);
@@ -21,11 +21,13 @@ function App() {
   return (
     <Router>
       <NavigationBar updateToken={updateToken} token={token} />
+      <PhotoGallery />
       <Routes>
         <Route path="/" />
         <Route path="/flows" />
       </Routes>
     </Router>
+    
   );
 }
 export default App;
