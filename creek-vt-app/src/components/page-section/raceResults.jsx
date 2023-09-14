1c388b43fb06eff5747ea542b84cedfc7c7e813f
JasonMackenzie808 <jasonmackenzie808@gmail.com>
2023-09-14 10:43:53 -0400

    updated the results page to take in racename from the store

--- creek-vt-app/src/components/page-section/raceResults.jsx
+++ creek-vt-app/src/components/page-section/raceResults.jsx
@@ -1,15 +1,19 @@
+import React, { useState, useContext } from 'react';
+
 import { API_VIEW_RESULTS_BY_RACENAME } from "../../constants/endpoints";
-import urlBuilder from "../../constants/urlBuilder";
+import urlBuilder from "../util/urlBuilder";
+import { UserContext } from '../store/UserContext';
 
 // race results filtered by race ID
 const RaceResults = (props) => {
+  const userctx = useContext(UserContext)
 async function fetchRaceResults() {
   const raceName = "New Haven Ledges Race"
   try {
     let requestOptions = {
       method: "GET",
     };
-    const response = await fetch(API_VIEW_RESULTS_BY_RACENAME + urlBuilder(raceName, requestOptions))
+    const response = await fetch(API_VIEW_RESULTS_BY_RACENAME + urlBuilder(userctx.race.name), requestOptions)
     const data = await response.json();
     console.log(data);
   } catch (error) {
