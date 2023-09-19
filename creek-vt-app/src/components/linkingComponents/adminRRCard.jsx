a362e9815faf88ad114bcd9c70d82f26fd745fe4
bronz411 <123334653+bronz411@users.noreply.github.com>
2023-09-19 11:17:14 -0600

    working on admin, still need to fix the add racer and update racer

--- creek-vt-app/src/components/linkingComponents/adminRRCard.jsx
+++ creek-vt-app/src/components/linkingComponents/adminRRCard.jsx
@@ -1,6 +1,63 @@
-// need to build out a card for each registered racer(use same stuff in dashboard to create.), and put delete logic here. need to figure out how to pull token properly from localstorage.
+import React, { useContext, useState } from "react";
+import { API_DELETE_REGISTERED_RACER } from "../../constants/endpoints";
+import { UserContext } from "../store/UserContext";
 const AdminRRCard = (props) => {
-  return <div></div>;
+  const userctx = useContext(UserContext);
+  let { id, firstName, lastName, email, raceId } = props.racer;
+
+  function handleDeleteRacerClick(clickValue) {
+    deleteRegisteredRacer(clickValue);
+  }
+  async function deleteRegisteredRacer(selectedRacer) {
+    try {
+      const token = localStorage.getItem("token");
+      const myHeaders = new Headers();
+      myHeaders.append("Authorization", token);
+      let requestOptions = {
+        method: "DELETE",
+        headers: myHeaders,
+      };
+      const response = await fetch(
+        API_DELETE_REGISTERED_RACER + raceId + "/" + selectedRacer,
+        requestOptions
+      );
+      const data = await response.json();
+      props.fetchRegisteredRacers();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <div
+        style={{
+          border: "black 1.5px solid",
+          margin: "2px 0",
+          justifyContent: "space-between",
+        }}
+        className="d-flex">
+        ID:{id}, Firstname: {firstName}, Lastname:
+        {lastName}, Email: {email}
+        <div className="d-flex flex-column m-1">
+          <button
+            onClick={() => handleDeleteRacerClick(email)}
+            style={{ height: "50%", backgroundColor: "red" }}>
+            DELETE
+          </button>
+          <button
+            onClick={() => userctx.setSelectedUpdateAthlete(props.racer)}
+            style={{
+              height: "50%",
+              backgroundColor: "blue",
+              color: "white",
+            }}>
+            UPDATE
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminRRCard;
