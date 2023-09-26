3d0d93a832ae4969703482e2d3afe5f97d953375
Alex Villanueva <138938590+liminalpup@users.noreply.github.com>
2023-09-26 13:46:52 -0400

    worked on map styling for course desciption page and fixed styling of footer to center admin dashboard button in mobile

--- creek-vt-app/src/components/page-section/raceMap.jsx
+++ creek-vt-app/src/components/page-section/raceMap.jsx
@@ -13,7 +13,7 @@ const RaceMap = (props) => {
     setPutIn();
   }, []);
   return (<>
-    <MapContainer center={putIn} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={putIn} zoom={13} scrollWheelZoom={false} >
       <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
