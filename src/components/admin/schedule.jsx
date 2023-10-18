15b362ff491573e2bf480107e2223025c1e09d40
Scott Gilbert <scottvtgilbert@gmail.com>
2023-10-17 21:24:49 -0400

    Adding functionality to schedule page - able to update schedule items, add new schedule item inputs, and remove schedule items

--- src/components/admin/schedule.jsx
+++ src/components/admin/schedule.jsx
@@ -7,9 +7,42 @@ import { formatDateTime } from "../../utils/formatDateTime";
 //Styles
 import styles from "./schedule.module.css"
 
-export default function Schedule({ racename }) {
+function ScheduleInput({ i, handleChange, handleRemove, formData }) {
+    return (
+        <section className={`${styles["schedule-input__section"]}`} id={`event-${i}`}>
+            <div className={"input-row"}>
+                <div className="input-group">
+                    <label htmlFor={`name-${i}`}>Name</label>
+                    <input type="text" name="name" id={`name-${i}`} onChange={handleChange} value={formData.schedule[i].name} />
+                </div>
+                <div className="input-group">
+                    <label htmlFor={`location-${i}`}>Location</label>
+                    <input type="text" name="location" id={`location-${i}`} onChange={handleChange} value={formData.schedule[i].location} />
+                </div>
+            </div>
+
+            <div className="input-row">
+                <div className={"input-group"}>
+                    <label htmlFor={`startTime-${i}`}>Start Time</label>
+                    <input type="time" name="startTime" id={`startTime-${i}`} onChange={handleChange} value={formData.schedule[i].startTime} />
+                </div>
+                <div className="input-group">
+                    <label htmlFor={`endTime-${i}`}>End Time</label>
+                    <input type="time" name="endTime" id={`endTime-${i}`} onChange={handleChange} value={formData.schedule[i].endTime} />
+                </div>
+            </div>
+            <button type="button" onClick={handleRemove}>Remove</button>
+        </section>
+    )
+}
+
+
+
+export default function Schedule() {
     const [formData, setFormData] = useState({});
+    const [scheduleInputs, setScheduleInputs] = useState([])
     const [selectedRace, setSelectedRace] = useContext(SelectedRaceContext)
+
 
     useEffect(() => {
         const getRaceData = async () => {
@@ -17,7 +50,6 @@ export default function Schedule({ racename }) {
                 const raceToFetch = selectedRace.split(' ').join('').toLowerCase();
                 let raceData = await fetch(`http://localhost:3000/races/${raceToFetch}`)
                 let raceJSON = await raceData.json();
-                console.log(JSON.parse(raceJSON[0].schedule))
                 setFormData({
                     date: formatDateTime(new Date(raceJSON[0].date)).htmlDateTime,
                     schedule: JSON.parse(raceJSON[0].schedule)
@@ -26,12 +58,21 @@ export default function Schedule({ racename }) {
             catch (err) {
                 setFormData({
                     date: null,
+                    schedule: null
                 })
 
             }
         }
         getRaceData()
     }, [selectedRace])
+
+    useEffect(() => {
+        setScheduleInputs(formData.schedule ? formData.schedule.map((scheduleItem, i) => {
+            return (
+                <ScheduleInput i={i} handleChange={handleChange} formData={formData} handleRemove={handleRemove}/>
+            )
+        }) : [])
+    }, [formData])
 
     console.log(formData)
 
@@ -57,10 +98,7 @@ export default function Schedule({ racename }) {
         let propertyName = e.target.name;
         setFormData(prev => {
             let updatedSchedule = prev.schedule.map((item, i) => {
-                console.log(i, itemIndex)
-                console.log(item)
                 if (i === itemIndex) {
-                    console.log(item)
                     item[propertyName] = e.target.value
                     return item
                 }
@@ -70,37 +108,25 @@ export default function Schedule({ racename }) {
         })
     }
 
+    function handleRemove(e){
+        console.log(e.target.parentElement)
+        setFormData(prev => {
+            let updatedSchedule = prev.schedule.filter((item,i)=>{
+                return i !== Number(e.target.parentElement.id.split('-')[1])
+            })
+            return {...prev, schedule: updatedSchedule}
+        })
 
-    let scheduleInputs = formData.schedule ? formData.schedule.map((scheduleItem, i) => {
-        return (
-            <section className={`${styles["schedule-input__section"]}`}>
-                <div className={"input-row"}>
-                    <div className="input-group">
-                        <label htmlFor={`name-${i}`}>Name</label>
-                        <input type="text" name="name" id={`name-${i}`} onChange={handleChange} value={formData.schedule[i].name} />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor={`location-${i}`}>Location</label>
-                        <input type="text" name="location" id={`location-${i}`} onChange={handleChange} value={formData.schedule[i].location} />
-                    </div>
-                </div>
+    }
 
-                <div className="input-row">
-                    <div className={"input-group"}>
-                        <label htmlFor={`startTime-${i}`}>Start Time</label>
-                        <input type="time" name="startTime" id={`startTime-${i}`} onChange={handleChange} value={formData.schedule[i].startTime} />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor={`endTime-${i}`}>End Time</label>
-                        <input type="time" name="endTime" id={`endTime-${i}`} onChange={handleChange} value={formData.schedule[i].endTime} />
-                    </div>
-                </div>
-            </section>
-        )
-    })
-        :
-        ''
-        ;
+    //By adding a new element to our schedule array in the form data our useEffect above will run and create a new list of our schedule inputs
+    function addScheduleInput() {
+        setFormData(prev => {
+            let updatedSchedule = prev.schedule.map(item => item)
+            updatedSchedule[updatedSchedule.length] = { "startTime": "", "endTime": "", "name": "", "location": "" }
+            return { ...prev, schedule: updatedSchedule }
+        })
+    }
 
     return (
         <div className="admin-edit__container">
@@ -109,8 +135,7 @@ export default function Schedule({ racename }) {
                 <div class={`${styles["schedule-inputs"]}`}>
                     {scheduleInputs}
                 </div>
-
-
+                <button type="button" onClick={addScheduleInput}>Add</button>
                 <button type="submit">Save</button>
             </form>
         </div>
