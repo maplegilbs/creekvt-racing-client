fae4b079e1e654e29b0063b8b4a77d5b00f68860
Scott Gilbert <scottvtgilbert@gmail.com>
2023-11-25 20:21:22 -0500

    Race styling updates

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 export default function FAQ() {
-    
-    
+    const [faqData, setFAQData] = useState(null)
+    const params = useParams()
+
+    useEffect(() => {
+        async function getFAQData() {
+            try {
+                const faqResponse = await fetch(`http://localhost:3000/faq/${params.raceName}`);
+                const faqJSON = await faqResponse.json();
+                setFAQData(faqJSON)
+            } catch (error) {
+                setFAQData([])
+                console.error(error)
+            }
+        }
+        getFAQData()
+    }, [])
+
     return (
-        <p>Get in touch with us here:</p>
-
-
-
+        <>
+            {faqData &&
+                <>
+                    {faqData.map(item => {
+                        return (<>
+                            <p><strong>{item.question}</strong></p>
+                            <p>{item.answer}</p>
+                        </>)
+                    })
+                    }
+                </>
+            }
+            <p>More questions? Get in touch with us here:</p>
+        </>
     )
 }
