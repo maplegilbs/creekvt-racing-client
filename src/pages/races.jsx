014a835fc31ab13018d7bc3b37c025b5c09e8b05
Scott Gilbert <scottvtgilbert@gmail.com>
2024-03-20 20:44:58 -0400

    Sorting race cards on race page by date

--- src/pages/races.jsx
+++ src/pages/races.jsx
@@ -14,43 +14,50 @@ export async function loader() {
 export default function Races() {
     const racesData = useLoaderData();
 
-    const racesCards = racesData.filter(raceDate => raceDate.isPublished).map(raceData => {
-        return (
-            <div key={raceData.name} className={`card ${styles["card"]}`}>
-                <h5 className={`card-title ${styles["card-title"]}`}>{raceData.name}</h5>
-                <img src={raceData.primaryImageURL} className={`card-img`} alt="..." />
-                <div className={`card-body ${styles["card-body"]}`}>
-                    <div className={`${styles["card-body-details"]}`}>
-                        <div className={`${styles["details-group"]}`}>
-                            <p>Next Race:</p> {new Date(raceData.date) > new Date() ? <p>{formatDateTime(new Date(raceData.date)).fullDate}</p> : <p>TBD</p>}
+    const racesCards = racesData.filter(raceData => raceData.isPublished)
+        .sort((race1, race2) => {
+            if (race1.date < race2.date) return -1;
+            if (race1.date > race2.date) return 1;
+            return 0
+        })
+        .map(raceData => {
+            console.log(raceData)
+            return (
+                <div key={raceData.name} className={`card ${styles["card"]}`}>
+                    <h5 className={`card-title ${styles["card-title"]}`}>{raceData.name}</h5>
+                    <img src={raceData.primaryImageURL} className={`card-img`} alt="..." />
+                    <div className={`card-body ${styles["card-body"]}`}>
+                        <div className={`${styles["card-body-details"]}`}>
+                            <div className={`${styles["details-group"]}`}>
+                                <p>Next Race:</p> {new Date(raceData.date) > new Date() ? <p>{formatDateTime(new Date(raceData.date)).fullDate}</p> : <p>TBD</p>}
+                            </div>
+                            <div className={`${styles["details-group"]}`}>
+                                <p>Race Type:</p> <p>{raceData.type}</p>
+                            </div>
+                            <div className={`${styles["details-group"]}`}>
+                                <p>Difficulty:</p> <p>{raceData.difficulty}</p>
+                            </div>
+                            <div className={`${styles["details-group"]}`}>
+                                <p>Format:</p> <p>{raceData.format}</p>
+                            </div>
                         </div>
-                        <div className={`${styles["details-group"]}`}>
-                            <p>Race Type:</p> <p>{raceData.type}</p>
+                        <p className={`card-text ${styles["card-text"]}`}>
+                            {raceData.shortDescription}
+                        </p>
+                        <div className={`${styles["button__container"]}`}>
+                            {(raceData.isRegOpen && (new Date(raceData.date) > new Date())) ?
+                                <a href={`${raceData.name.split(' ').join('').toLowerCase()}/register`} className={`button button--large`}>
+                                    Register &nbsp;<img src="https://creekvt.com/races/RacerIcon.png" />
+                                </a>
+                                :
+                                <></>
+                            }
+                            <a href={`/races/${raceData.name.split(' ').join('').toLowerCase()}`} className={`button button--large`}>Information</a>
                         </div>
-                        <div className={`${styles["details-group"]}`}>
-                            <p>Difficulty:</p> <p>{raceData.difficulty}</p>
-                        </div>
-                        <div className={`${styles["details-group"]}`}>
-                            <p>Format:</p> <p>{raceData.format}</p>
-                        </div>
-                    </div>
-                    <p className={`card-text ${styles["card-text"]}`}>
-                        {raceData.shortDescription}
-                    </p>
-                    <div className={`${styles["button__container"]}`}>
-                        {(raceData.isRegOpen && (new Date(raceData.date) > new Date())) ?
-                            <a href={`${raceData.name.split(' ').join('').toLowerCase()}/register`} className={`button button--large`}>
-                                Register &nbsp;<img src="https://creekvt.com/races/RacerIcon.png" />
-                            </a>
-                            :
-                            <></>
-                        }
-                        <a href={`/races/${raceData.name.split(' ').join('').toLowerCase()}`} className={`button button--large`}>Information</a>
                     </div>
                 </div>
-            </div>
-        )
-    })
+            )
+        })
 
     return (
         <main>
@@ -66,8 +73,8 @@ export default function Races() {
                         this site you can register to compete, find information on each
                         race, or relive prior years by browsing through our collection of race
                         results and photos. For the 2024 season we are looking forward
-                        to the return of the classic New Haven Ledges Race, as well as the 3rd
-                        annual Peavine Race. Check the individual race pages for details and to
+                        to the classic New Haven Ledges Race, the 3rd
+                        annual Peavine Race as well as the return of the Wells River Rumble! Check the individual race pages for details and to
                         sign up. </p>
                     <p className={`${styles["heading-container__summary"]}`}>- See you at the finish line!</p>
                 </div>
