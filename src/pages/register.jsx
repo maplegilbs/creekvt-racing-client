ce6a1510c181aedae89fbcf30050b98d202aaa54
Scott Gilbert <scottvtgilbert@gmail.com>
2024-03-28 20:07:56 -0400

    Updating register and checkout pages and components for youth fee

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -42,6 +42,7 @@ export default function Register() {
     const [receiptInfo, setReceiptInfo] = useState(null)
     const navigate = useNavigate()
 
+
     let racerCategoryOptions =
         raceInfo.categoryOptions ?
             raceInfo.categoryOptions.split(", ").map(category => <option value={category}>{category}</option>)
@@ -106,53 +107,59 @@ export default function Register() {
     else {
         return (
             <>
-            {checkoutStatus === null &&
-                <div className={`${styles["registration-form__container"]}`}>
-                    <h4 className={`section-heading`}>Register for the {`${currentRaceYear} ${raceInfo.name}`}</h4>
-                    <br />
-                    <div className={`${styles["header-info"]}`}>
-                        <p className={`${styles["race-details"]}`}><strong>Race Date:</strong></p><p>{`${formatDateTime(new Date(raceInfo.date)).fullDate}`}</p>
-                    </div>
-                    <div className={`${styles["header-info"]}`}>
-                        <p className={`${styles["race-details"]}`}><strong>Fee:</strong> </p><p>{`$${raceInfo.fee} per racer`}</p>
-                    </div>
-                    {raceInfo.acaDiscount > 0 &&
+                {checkoutStatus === null &&
+                    <div className={`${styles["registration-form__container"]}`}>
+                        <h4 className={`section-heading`}>Register for the {`${currentRaceYear} ${raceInfo.name}`}</h4>
+                        <br />
                         <div className={`${styles["header-info"]}`}>
-                            <p className={`${styles["race-details"]}`}><strong>ACA Member Discount:</strong> </p><p>{`$${raceInfo.acaDiscount}`}</p>
+                            <p className={`${styles["race-details"]}`}><strong>Race Date:</strong></p><p>{`${formatDateTime(new Date(raceInfo.date)).fullDate}`}</p>
                         </div>
-                    }
-                    <br />
-                    <p className={`${styles["required-notice"]}`}>Required fields are denoted with an asterisk *</p>
-                    <form onSubmit={handleSubmit}>
-                        {racerCategoryOptions &&
-                            <div className={`input-row ${styles["registration-row"]}`}>
-                                <div className={`input-group ${styles["registration-group"]}`}>
-                                    <div><label htmlFor="category">Category&nbsp;</label><span className="required__span">*</span></div>
-                                    <select required value={registrationFormData.category} onChange={(e) => setRegistrationFormData(prev => { return { ...prev, category: e.target.value } })} name="category" id="category">
-                                        <option value=""></option>
-                                        {racerCategoryOptions}
-                                    </select>
-                                </div>
+                        <div className={`${styles["header-info"]}`}>
+                            <p className={`${styles["race-details"]}`}><strong>Fee:</strong> </p><p>{`$${raceInfo.fee} per racer`}</p>
+                        </div>
+                        {raceInfo.youthFee &&
+                            <div className={`${styles["header-info"]}`}>
+                                <p className={`${styles["race-details"]}`}><strong>Youth (Under 18) Fee:</strong> </p><p>{`$${raceInfo.youthFee} per racer`}</p>
                             </div>
                         }
-                        {(canAddPartners[raceName] && craftWithMultipleRacers.includes(registrationFormData.category)) &&
-                            <p>Register once per boat.  Be sure to include all racing partners who will be in the boat below.</p>
+                        {raceInfo.acaDiscount > 0 &&
+                            <div className={`${styles["header-info"]}`}>
+                                <p className={`${styles["race-details"]}`}><strong>ACA Member Discount:</strong> </p><p>{`$${raceInfo.acaDiscount}`}</p>
+                            </div>
                         }
-                        <hr />
-                        {(registrationFormData.racers && registrationFormData.racers.length > 0) &&
-                            registrationFormData.racers.map((racer, racerIndex) => {
-                                return (
-                                    <RacerRow key={racerIndex} raceInfo={raceInfo} setRegistrationFormData={setRegistrationFormData} registrationFormData={registrationFormData} raceName={raceName} racerIndex={racerIndex} />
 
-                                )
-                            })
-                        }
-                        {(canAddPartners[raceName] && craftWithMultipleRacers.includes(registrationFormData.category)) &&
-                            <button className={`${styles["add-partner__button"]}`} type="button" onClick={addPartner}><FontAwesomeIcon icon={faCirclePlus} />&nbsp;Add Partner</button>
-                        }
-                        <button className={`button button--medium`} type="submit">Continue to Payment</button>
-                    </form>
-                </div>
+                        <br />
+                        <p className={`${styles["required-notice"]}`}>Required fields are denoted with an asterisk *</p>
+                        <form onSubmit={handleSubmit}>
+                            {racerCategoryOptions &&
+                                <div className={`input-row ${styles["registration-row"]}`}>
+                                    <div className={`input-group ${styles["registration-group"]}`}>
+                                        <div><label htmlFor="category">Category&nbsp;</label><span className="required__span">*</span></div>
+                                        <select required value={registrationFormData.category} onChange={(e) => setRegistrationFormData(prev => { return { ...prev, category: e.target.value } })} name="category" id="category">
+                                            <option value=""></option>
+                                            {racerCategoryOptions}
+                                        </select>
+                                    </div>
+                                </div>
+                            }
+                            {(canAddPartners[raceName] && craftWithMultipleRacers.includes(registrationFormData.category)) &&
+                                <p>Register once per boat.  Be sure to include all racing partners who will be in the boat below.</p>
+                            }
+                            <hr />
+                            {(registrationFormData.racers && registrationFormData.racers.length > 0) &&
+                                registrationFormData.racers.map((racer, racerIndex) => {
+                                    return (
+                                        <RacerRow key={racerIndex} raceInfo={raceInfo} setRegistrationFormData={setRegistrationFormData} registrationFormData={registrationFormData} raceName={raceName} racerIndex={racerIndex} />
+
+                                    )
+                                })
+                            }
+                            {(canAddPartners[raceName] && craftWithMultipleRacers.includes(registrationFormData.category)) &&
+                                <button className={`${styles["add-partner__button"]}`} type="button" onClick={addPartner}><FontAwesomeIcon icon={faCirclePlus} />&nbsp;Add Partner</button>
+                            }
+                            <button className={`button button--medium`} type="submit">Continue to Payment</button>
+                        </form>
+                    </div>
                 }
                 {checkoutStatus === 'pending' &&
                     <Checkout registrationData={registrationFormData} raceName={raceName} raceInfo={raceInfo} setCheckoutStatus={setCheckoutStatus} setReceiptInfo={setReceiptInfo} />
